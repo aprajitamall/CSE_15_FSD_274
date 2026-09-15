7737fcccca8ec9a4dc72719b7fa17beed8845928
Aprajita Mall <aprajitamall2709@gmail.com>
2026-09-15 11:21:18 +0530

    synchronous and asynchronous

---
+++ JS/async.js
@@ -0,0 +1,12 @@
+function sum(){
+setTimeout(()=>{
+console.log("2");
+},2000);} 
+
+function example(){
+console.log("1");
+
+sum();
+console.log("3");
+}
+example();
---
+++ JS/sync.js
@@ -0,0 +1,12 @@
+function sum()
+{
+console.log("2");
+}
+
+function example(){
+console.log("1");
+sum();
+console.log("3");
+}
+
+example();
