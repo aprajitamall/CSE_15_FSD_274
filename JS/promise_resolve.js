ff31d4954128b11581b67ca791e90d3709c4138c
Aprajita Mall <aprajitamall2709@gmail.com>
2026-09-15 11:45:07 +0530

    promise (resolve)

---
+++ JS/promise_resolve.js
@@ -0,0 +1,14 @@
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("found data");
+        
+    }, 2000);
+});
+
+promise.then((result) => {
+    console.log(result);
+});
+
+promise.catch((error) => {
+    console.log(error);
+});
